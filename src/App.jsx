4415d94cedd8ad7a71c7e8bef3d7f732f37a6242
Joshua Kisb <joshuakisb@gmail.com>
2025-03-28 13:41:02 +0300

    add antd and program select

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,91 @@
-import { useDataQuery } from '@dhis2/app-runtime'
-import i18n from '@dhis2/d2-i18n'
-import React from 'react'
-import classes from './App.module.css'
+import { useDataQuery } from "@dhis2/app-runtime";
+import i18n from "@dhis2/d2-i18n";
+import React, { useState } from "react";
+import classes from "./App.module.css";
+import { Button, DatePicker, Form, Input, Select, TreeSelect } from "antd";
 
 const query = {
-    me: {
-        resource: 'me',
+	me: {
+		resource: "me",
+	},
+    programs: {
+        resource: "programs"
     },
-}
+    orgs: {
+		// userOrgUnits
+		resource: "organisationUnits.json",
+		params: {
+			paging: "false",
+			fields: "id,name,path,leaf,level,parent[id]",
+		},
+	},
+};
+
+const { RangePicker } = DatePicker;
 
 const MyApp = () => {
-    const { error, loading, data } = useDataQuery(query)
+	const { error, loading, data } = useDataQuery(query);
+	const [form] = Form.useForm();
+	// const [formLayout, setFormLayout] = useState("sample");
 
-    if (error) {
-        return <span>{i18n.t('ERROR')}</span>
-    }
+	if (error) {
+		return <span>{i18n.t("ERROR")}</span>;
+	}
 
-    if (loading) {
-        return <span>{i18n.t('Loading...')}</span>
-    }
+	if (loading) {
+		return <span>{i18n.t("Loading...")}</span>;
+	}
+    const programs = data.programs.programs.map(p => ({ value: p.id, label: <span>{p.displayName}</span> }));
+    const userOrgUnits = data.orgs.organisationUnits.map((o) => ({
+        ...o,
+        pId: o.parent?.id,
+    })); 
+    const units = userOrgUnits.map((unit) => {
+        return {
+            id: unit.id,
+            pId: unit.pId || "",
+            value: unit.id,
+            title: unit.name,
+            isLeaf: unit.leaf,
+        };
+    });
 
-    return (
-        <div className={classes.container}>
-            <h1>{i18n.t('Hello {{name}}', { name: data.me.name })}</h1>
-            <h3>{i18n.t('Welcome to DHIS2!')}</h3>
-        </div>
-    )
-}
+	return (
+		<div className={classes.container}>
+			<Form
+				layout={`inline`}
+				form={form}
+				initialValues={{ }}
+				onValuesChange={() => {}}
+				style={{ maxWidth: "none" }}
+			>
+				<Form.Item label="Program" name="program">
+					<Select
+                        style={{minWidth: "350px"}}
+						options={programs}
+					/>
+				</Form.Item>
+				<Form.Item label="Orgunit">
+                <TreeSelect
+                    showSearch
+                    style={{ width: '100%' }}
+                    value={value}
+                    dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
+                    placeholder="Please select"
+                    allowClear
+                    onChange={() => {}}
+                    treeData={units}
+                    />
+				</Form.Item>
+				<Form.Item label="Date Range">
+                    <RangePicker />
+				</Form.Item>
+				<Form.Item>
+					<Button type="primary">Submit</Button>
+				</Form.Item>
+			</Form>
+		</div>
+	);
+};
 
-export default MyApp
+export default MyApp;
